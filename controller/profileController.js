28ec573e5550d3ed8b6f2fe56a2ccfd5c1fd677e
AquaApple <yassershehab9@gmail.com>
2022-05-18 17:14:28 +0200

    Added Create Profile

--- controller/profileController.js
+++ controller/profileController.js
@@ -8,43 +8,6 @@ const createProfile = asyncHandler(async (req, res) => {
   res.send(profile);
 });
 
-//Edit Profile
-//@REQUEST PUT
-const editProfile = asyncHandler(async (req, res) => {
-  const profile = await Profile.findById(req.params.id);
-  if (!profile) {
-    res.status(400);
-    throw new Error("Profile not found");
-  }
-  const updatedProfile = await Profile.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-  });
-  res.status(200).json(updatedProfile);
-});
-//Get Single Profile
-//@REQUEST GET
-const getProfile = asyncHandler(async (req, res) => {
-  //Validates the Id params before the Query
-  if (req.params.id.match(/^[0-9a-fA-F]{24}$/)) {
-    const profile = await Profile.findById(req.params.id);
-    if (!profile) {
-      res.status(400);
-      throw new Error("Profile not found");
-    } else {
-      res.status(200).json(profile);
-    }
-  }
-  res.status(400);
-  throw new Error("Profile ID format Error");
-});
-//Get All Profiles
-//@REQUEST GET
-const getAllProfiles = asyncHandler(async (req, res) => {
-  const profiles = await Profile.find({});
-
-  res.status(200).json(profiles);
-});
-
 module.exports = {
   createProfile,
   editProfile,
